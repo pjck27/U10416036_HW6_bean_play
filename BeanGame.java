5daf117e64c6c19fc8aac48c8ddc9da58b2bf941
pjck27 <pjck27@gmail.com>
2016-04-05 15:37:20 +0800

    Add BallPane and Drawer into scene

--- BeanGame.java
+++ BeanGame.java
@@ -1,47 +1,60 @@
 //U10416036
 
-import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.beans.property.DoubleProperty;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.scene.shape.*;
-import javafx.scene.paint.Color;
-import javafx.util.Duration;
+import javafx.scene.text.Text;
 /**
  * @author MengLin
  */
 
 public class BeanGame extends Application {
-    @Override
+    //@Override
+    int n = 0 ;
+	
     public void start(Stage primaryStage) {
-        //Create a scene and place it in the stage
-        Scene scene = new Scene(new Drawer(), 300, 300);
-        //Set the stage title
-        primaryStage.setTitle("BeanGame");
-        //Place the scene in the stage
+        BallPane b = new BallPane();
+        
+        Text text = new Text(20, 20, "");
+
+        b.setOnMouseMoved(e -> {
+            text.setText("x = " + e.getX() + "\n" + "y = " + e.getY());
+        });
+        
+        b.setOnMousePressed(e -> b.pause());
+        b.setOnMouseReleased(e -> b.play());
+        
+        Drawer drawer = new Drawer();
+
+        Scene scene = new Scene(new StackPane(b),300,300);
+
         primaryStage.setScene(scene);
-        //Display the stage
+        b.getChildren().add(drawer);
+        b.getChildren().add(text);
+        primaryStage.setTitle("Bean Machine");
         primaryStage.show();
+
+       
+    
+            
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
+
 class Drawer extends Pane{
     public final double radius = 3;
     private double x = radius, y = radius;
     private double dx = 0, dy = 1;
     private Timeline animation;
-    private Circle ball = new Circle(x, y, radius);
     
     public Drawer(){
         int slot = 8;
@@ -54,13 +67,8 @@ class Drawer extends Pane{
         Line l6 = new Line(240,250,170,85);
         Line l7 = new Line(170,85,170,55);
 
-        getChildren().add(l1);
-        getChildren().add(l2);
-        getChildren().add(l3);
-        getChildren().add(l4);
-        getChildren().add(l5);
-        getChildren().add(l6);
-        getChildren().add(l7);
+        getChildren().addAll(l1,l2,l3,l4,l5,l6,l7);
+        
         
         //Calculate how many slot at bottom
         double bottom = ((240 - 60) / slot);
@@ -85,37 +93,6 @@ class Drawer extends Pane{
             }
         }
         
-        ball.setFill(Color.BLACK);
-        getChildren().add(ball);
-        animation = new Timeline(new KeyFrame(Duration.millis(20),e -> moveBall()));
-        animation.setCycleCount(Timeline.INDEFINITE);
-        animation.play();
-    }
-    public void play(){
-        animation.play();
     }
     
-    public void pause(){
-        animation.pause();
-    }
-    
-    
-    public DoubleProperty rateProperty(){
-        return animation.rateProperty();
-    }
-    
-    protected void moveBall(){
-        if(x < radius || x > getWidth() - radius){
-            dx *= -1;
-        }
-        if(y < radius || y > getHeight() - radius){
-            dy *= -1;
-        }
-    
-        x += dx;
-        y += dy;
-        
-        ball.setCenterX(x);
-        ball.setCenterY(y);
-    }
 }
